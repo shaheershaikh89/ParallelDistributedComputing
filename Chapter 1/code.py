f24977a3c2db8007527252ace5de5ca3d61735c6
shaheershaikh89 <shaheershaikh89@gmail.com>
2025-11-08 07:23:31 +0500

    Chapter 1 commit

---
+++ Chapter 1/code.py
@@ -0,0 +1,4 @@
+def do_something(size, out_list):
+
+    for i in range(size):
+        out_list.append(i * i)
---
+++ Chapter 1/process_Thread.py
@@ -0,0 +1,50 @@
+from do_something import do_something
+import time
+import multiprocessing
+import threading
+
+if __name__ == "__main__":
+    size = 10000000  # workload
+    counts = [5, 10, 15]  # counts for pocesses and threads to check for each
+
+    print("\n====== Multiprocessing ======")
+    for procs in counts:
+        jobs = []
+        start_time = time.time()
+
+        # Create and start processes
+        for i in range(procs):
+            out_list = []
+            process = multiprocessing.Process(target=do_something, args=(size, out_list))
+            jobs.append(process)
+
+        for p in jobs:
+            p.start()
+
+        for p in jobs:
+            p.join()
+
+        end_time = time.time()
+        print(f"Processes: {procs}, Time taken = {end_time - start_time:.4f} seconds")
+
+    print("\n====== Multithreading ======")
+    for threads in counts:
+        jobs = []
+        start_time = time.time()
+
+        # Create and start threads
+        for i in range(threads):
+            out_list = []
+            thread = threading.Thread(target=do_something, args=(size, out_list))
+            jobs.append(thread)
+
+        for t in jobs:
+            t.start()
+
+        for t in jobs:
+            t.join()
+
+        end_time = time.time()
+        print(f"Threads: {threads}, Time taken = {end_time - start_time:.4f} seconds")
+
+    print("\nDone with all")
